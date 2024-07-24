d86b5f5159ce3012ce6ca9fc6531ac169fc16567
Luke Honeyball <luke.honeyball@protonmail.com>
2024-07-24 14:30:24 +0200

    Update hidden.js

--- hidden.js
+++ hidden.js
@@ -1,66 +1,39 @@
 function startup(){
-	console.log("started");
+
 	chrome.storage.local.set({citelist: ""},function(){});
 	chrome.storage.local.set({sessionon: false},function(){});
 	chrome.storage.local.set({urlgot: ""},function(){});
-	chrome.webNavigation.onCommitted.addListener(function(newnav){
-				console.log("new website nav");
-				chrome.storage.local.get("sessionon",function(ret){
-					if(ret.sessionon){
-						var us = newnav.url.split(":")[0];
-						console.log(newnav.url);
-						if(us == "https" || us == "http"){
-								chrome.scripting.executeScript(
-								{
-									target: { tabId: newnav.tabId },files: ['grabcontent.js']
-								});
-						}
-					}
-				});
-		
-	});
+	startup_otherfunc();
 }
+function startup_otherfunc(){
+	
+		chrome.webNavigation.onCommitted.addListener(function(newnav){
+					
+					chrome.storage.local.get("sessionon",function(ret){
+						if(ret.sessionon){
+							var us = newnav.url.split(":")[0];
 
+							if(us == "https" || us == "http"){
+									chrome.scripting.executeScript(
+									{
+										target: { tabId: newnav.tabId },files: ['grabcontent.js']
+									});
+							}
+						}
+					});
+					
+			
+		});
+	}
 chrome.runtime.onStartup.addListener(startup);
 chrome.runtime.onInstalled.addListener(startup);
 chrome.windows.onFocusChanged.addListener(function(){
-		chrome.webNavigation.onCommitted.addListener(function(newnav){
-				console.log("new website nav");
-				chrome.storage.local.get("sessionon",function(ret){
-					if(ret.sessionon){
-						var us = newnav.url.split(":")[0];
-						console.log(newnav.url);
-						if(us == "https" || us == "http"){
-								chrome.scripting.executeScript(
-								{
-									target: { tabId: newnav.tabId },files: ['grabcontent.js']
-								});
-						}
-					}
-				});
-		});
+	startup_otherfunc();
 });
 //dont insult the code please
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	console.log("got attribs");
 	if (message.type === 'WAKEUPBOZO') {
-		console.log("okok i woke up chill");
-		chrome.webNavigation.onCommitted.addListener(function(newnav){
-				console.log("new website nav");
-				chrome.storage.local.get("sessionon",function(ret){
-					if(ret.sessionon){
-						var us = newnav.url.split(":")[0];
-						console.log(newnav.url);
-						if(us == "https" || us == "http"){
-								chrome.scripting.executeScript(
-								{
-									target: { tabId: newnav.tabId },files: ['grabcontent.js']
-								});
-						}
-					}
-				});
-		
-	});
+		startup_otherfunc();
 	}
   if (message.type === 'DOCUMENT_ATTRIBUTES') {
 	chrome.storage.local.get("citelist", function(returned){
@@ -70,11 +43,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 		if (returned.citelist.includes(urlfrom)){
 			chrome.storage.local.set({citelist: returned.citelist},function(){});
 		}else{
-			chrome.storage.local.set({citelist: `${message.data.author}, (${message.data.date}), ${message.data.title} [online] Available from: < ${urlfrom} > (Accessed on: ${currdate.getDate()}/${month}/${currdate.getFullYear()})\n${returned.citelist}`},function(){}); 
+			chrome.storage.local.set({citelist: 
+			`${message.data.author}, `+
+			`(${message.data.date}), `+
+			`${message.data.title} ` +
+			`[online] Available from: < ${urlfrom} >` +
+			` (Accessed on: ${currdate.getDate()}/${month}/${currdate.getFullYear()})\n`+
+			`${returned.citelist}`},function(){}); 
 		}
 		
 		
 	});
   }
-});//totally normal code u know?
+});
+//totally normal code u know?
 //ps: u shouldn't be here
