2869b6501f6c60c08a9f10667b8619b8e0c9e8d1
Luke Honeyball <luke.honeyball@protonmail.com>
2024-07-24 17:57:04 +0200

    Stable Update
    improvements:
    -better author identification
    -better string manipulation
    -code cleanup

--- grabcontent.js
+++ grabcontent.js
@@ -3,23 +3,35 @@ var authorName = document.URL.split("/")[2].replace("www.","");
 var date = cur.getFullYear().toString();
 
 var doc_elements = document.querySelectorAll("*");
+var main_author = false;
 doc_elements.forEach(function(ele) {
 	for(var i = ele.attributes.length - 1; i >= 0; i--){
 		var atr = ele.attributes[i];
 		if(atr.specified){
-			if(atr.value.includes("author") && atr.value.includes("authori") == false){
-				console.log("found author");
-				if(ele.textContent.trim().length < 25 && ele.textContent.trim() != ""){
-					authorName = ele.textContent.trim();
+			if(main_author == false){
+				if(atr.value.includes("author") && atr.value.includes("authori") == false){
+					console.log("found author");
+					if(ele.textContent.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '').trim().length < 50 && ele.textContent.trim() != ""){
+						authorName = ele.textContent.trim();
+						console.log("author changed");
+						if(atr.value.includes("main")){
+							main_author = true;
+						}
+					}
+					
 				}
-			}
-			if(atr.value.includes("byline")){
-				if(ele.textContent.trim().length < 25 && ele.textContent.trim() != ""){
-					authorName = ele.textContent.trim();
+				if(atr.value.includes("byline")){
+					if(ele.textContent.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '').trim().length < 50 && ele.textContent.trim() != ""){
+						authorName = ele.textContent.trim();
+						console.log("author changed");
+						if(atr.value.includes("main")){
+							main_author = true;
+						}
+					}
 				}
 			}
 			if(atr.value.includes("date")){
-				if(ele.textContent.trim().length > 3 && ele.textContent.trim().length < 25 && ele.textContent.trim() != "" && /\d/.test(ele.textContent) == true){
+				if(ele.textContent.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '').trim().length > 3 && ele.textContent.trim().length < 25 && ele.textContent.trim() != "" && /\d/.test(ele.textContent) == true){
 					date = ele.textContent.trim();
 				}
 			}
